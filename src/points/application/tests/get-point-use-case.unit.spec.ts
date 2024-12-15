e8597b8e8f1c75cccf5a51ee98604df5d343781c
kiyoung92 <go3go1go2@gmail.com>
2024-12-15 22:42:38 +0900

    test: point unit test
    잔액 조회, 충전 단위 테스트 진행
    - controller
    - use case
    - service

--- src/points/application/tests/get-point-use-case.unit.spec.ts
+++ src/points/application/tests/get-point-use-case.unit.spec.ts
@@ -6,7 +6,7 @@ import { PointEntity } from 'src/points/domain/entities/point.entity';
 import { PointDomainModule } from 'src/points/domain/point-domain.module';
 import { PointService } from 'src/points/domain/services/point.service';
 
-describe('Get Point UseCase', () => {
+describe('GetPointUseCase', () => {
   let getPointUseCase: GetPointUseCase;
   let pointService: PointService;
 
@@ -19,7 +19,7 @@ describe('Get Point UseCase', () => {
     pointService = module.get<PointService>(PointService);
   });
 
-  it('should be defined', () => {
+  it('정의 확인', () => {
     expect(getPointUseCase).toBeDefined();
     expect(pointService).toBeDefined();
   });
@@ -27,33 +27,39 @@ describe('Get Point UseCase', () => {
   const mockDate = new Date().toISOString();
   jest.spyOn(Date.prototype, 'toISOString').mockReturnValue(mockDate);
 
-  it('성공', async () => {
-    const serviceMockResult = new PointEntity({
-      id: 1,
-      userId: 1,
-      point: 100,
-      updatedAt: '11',
+  describe('성공', () => {
+    it('포인트 조회 성공', async () => {
+      const serviceMockResult = new PointEntity({
+        id: 1,
+        userId: 1,
+        point: 100,
+        updatedAt: '11',
+      });
+
+      jest.spyOn(pointService, 'get').mockResolvedValueOnce(serviceMockResult);
+
+      const result = await getPointUseCase.execute({ userId: 1 });
+
+      expect(result).toEqual(serviceMockResult);
+      expect(pointService.get).toBeCalledWith({ userId: 1 });
     });
-
-    jest.spyOn(pointService, 'get').mockResolvedValueOnce(serviceMockResult);
-
-    const result = await getPointUseCase.execute({ userId: 1 });
-
-    expect(result).toEqual(serviceMockResult);
-    expect(pointService.get).toBeCalledWith({ userId: 1 });
   });
 
-  it('실패: 사용자 정보 없음', async () => {
-    jest
-      .spyOn(pointService, 'get')
-      .mockRejectedValueOnce(new NotFoundException('사용자 정보가 없습니다.'));
+  describe('실패', () => {
+    it('실패: 사용자 정보 없음', async () => {
+      jest
+        .spyOn(pointService, 'get')
+        .mockRejectedValueOnce(
+          new NotFoundException('사용자 정보가 없습니다.'),
+        );
 
-    try {
-      await getPointUseCase.execute({ userId: 0 });
-      fail('테스트 실패');
-    } catch (error) {
-      expect(error).toBeInstanceOf(NotFoundException);
-      expect(error.message).toEqual('사용자 정보가 없습니다.');
-    }
+      try {
+        await getPointUseCase.execute({ userId: 0 });
+        fail('테스트 실패');
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('사용자 정보가 없습니다.');
+      }
+    });
   });
 });
